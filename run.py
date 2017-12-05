6beaa3bd6278a7ebebc66b47b7dec0fa9371414f
wtewalt <wtewalt@gmail.com>
2017-12-05 16:08:33 -0500

    [comments]

--- run.py
+++ run.py
@@ -14,50 +14,13 @@ from functions import return_ga_data, clean_df_columns, format_all_dates
 
 # get data
 
-""" Notes
-
-
-** View IDs **
-
-Note: View IDs are used as strings in the code below.
-
-www.cspire.com = '84446473'
-    * Business-only = '141075452'
-Credera = '146168974'
-Home Service = '154585144'
-Home Services(Legacy)= '141114926'
-Prepaid = '159884119'
-Raw Data = '84454783'
-Wireless 04282017 = '149218469'
-Wireless Only = '141562611'
-
-
-
-** Data to add **
-
-Sesions Data:
-    new users [DONE]
-    users [DONE]
-    sessions [DONE]
-    session duration
-    bounces [DONE]
-    source [DONE]
-    medium [DONE]
-    campaign [DONE]
-    ad-content [DONE]
-    keyword [DONE]
-    goal completions(1-6?)
-
-"""
-
-
 
 # get sessions data.
 
 sessions_df = return_ga_data(
     start_date='2017-11-30',
     end_date='2017-11-30',
-    view_id='84446473',
+    view_id='<view id>',
     metrics=[{'expression': 'ga:sessions'}, 
              {'expression': 'ga:users'}, 
              #{'expression': 'ga:newUsers'},
@@ -79,9 +42,7 @@ sessions_df = return_ga_data(
                 #{'name': 'ga:userType'},
                 #{'name': 'ga:deviceCategory'}
                 ],
-    segments=[{"segmentId": "gaid::GvB1BxZNQDGKw3i3JTzBZw"}, # Business
-              {"segmentId": "gaid::h7S_Jb8iS_aPbQjsbKnJzw"}, # Consumer
-              {"segmentId": "gaid::-1"} # All Users
+    segments=[{"segmentId": "<gaid>"}
               ],
     split_dates=True,
     group_by=[]
@@ -104,31 +65,3 @@ Print the df shape to get a sense of rows per day returned.
 # print(sessions_df[:10]) # sessions_df[:4]
 # print(sessions_df.shape)
 # sessions_df.to_csv('c:/Users/wtewalt/Documents/ga_biweekly_data.csv')
-
-
-
-""" Creating the Pages-level Query
-
-pages_df = return_ga_data(
-    start_date='2017-09-20',
-    end_date='2017-09-20',
-    view_id=  '84446473',
-    metrics=[{'expression': 'ga:sessions'},
-             {'expression': 'ga:users'},
-             {'expression': 'ga:newUsers'},
-             {'expression': 'ga:bounces'}, ],
-    dimensions=[{'name': 'ga:date'},
-                #{'name': 'ga:hostname'},
-                #{'name': 'ga:pagePath'},
-                {'name': 'ga:pageTitle'},
-                {'name': 'ga:landingPagePath'},
-                {'name': 'ga:exitPagePath'}],
-    split_dates=True,
-    group_by=[]
-)
-
-pages_df = clean_df_columns(pages_df)
-pages_df['date'] = format_all_dates(pages_df.date)
-print(pages_df[:4])
-print(pages_df.shape)
-"""
